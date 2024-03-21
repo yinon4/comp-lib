96beec1a5608bade44d36cb623658e18bb129933
yinon <yinongilad4@gmail.com>
2024-03-21 18:46:29 +0200

    add new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { TabsDemo } from "@/components/examples/tabs";
 import { DatePicker } from "@/components/ui/date-picker";
 import { DatePickerWithRange } from "@/components/ui/date-range-picker";
 
 export default () => {
   return (
     <main className="flex flex-col justify-around items-center p-24 min-h-screen">
+      <TabsDemo />
       <DatePicker />
       <DatePickerWithRange />
     </main>
